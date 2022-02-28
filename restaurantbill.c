3662ebab51e60db723fc592a57d4b00f8bcbba72
japfeifroth <89482008+japfeifroth@users.noreply.github.com>
2022-02-28 13:26:26 -0800

    outline

---
+++ restaurantbill.c
@@ -0,0 +1,8 @@
+/*
+Create main 
+Declare variables: meal (meal cost), tax, tip, and total (total cost)
+Scanner objects to take the inputs (meal, tax, tip)
+Tax and the tip will be calculated
+Total cost will be calculated
+*/
+
