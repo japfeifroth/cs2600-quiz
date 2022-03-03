75fe39d6fd32a778eac2e0a0088ae7ff804c8a25
japfeifroth <89482008+japfeifroth@users.noreply.github.com>
2022-03-02 21:20:07 -0800

    outline

---
+++ numberguessinggame.c
@@ -0,0 +1,10 @@
+/*
+Declare variable
+while loop for the game (choice 1 and 2)
+Ask for choices
+Generate number to guess
+Compare numbers and see if game is won
+If game is won, go back to choices
+If 3 is entered, while loop stops
+Game ends
+*/
